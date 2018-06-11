e75d5ab28f96a90af73b6964c3bf65aac3e8e837
weixue meng <mengdong1101@gmail.com>
2018-06-11 15:34:24 +0800

    提交

---
+++ app/src/main/java/com/softnobug/m/MainActivity.java
@@ -0,0 +1,27 @@
+package com.softnobug.m;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.softnobug.switchbtnmodule.SwitchsButton;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        SwitchsButton switchsBtn = findViewById(R.id.swiftBtn);
+//
+//        ArrayList<String> list = new ArrayList<>();
+//        list.add("正在进行");
+//        list.add("已经关闭");
+//        list.add("剩下一个");
+//        switchsBtn.showList(list);
+
+    }
+
+}
